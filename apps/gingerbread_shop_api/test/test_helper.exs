26e6a62956d72f5b909e0d740e516c15f2b46fc0
Stefan Johnson <sj@scrimpycat.io>
2017-10-05 03:47:16 +1100

    Fix test setup

--- apps/gingerbread_shop_api/test/test_helper.exs
+++ apps/gingerbread_shop_api/test/test_helper.exs
@@ -1,1 +1,6 @@
+Application.ensure_all_started(:gingerbread_shop_service)
+Application.ensure_all_started(:ecto)
+
 ExUnit.start()
+
+Ecto.Adapters.SQL.Sandbox.mode(GingerbreadShop.Service.Repo, :manual)
